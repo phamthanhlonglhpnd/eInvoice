7fe748dd1ded8abd111e32eaa53734efec4c592a
Sotatek-LongPham2 <long.pham2@sotatek.com>
2026-03-30 21:59:45 +0700

    set up prettier

--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -7,36 +7,47 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { Logger as PinoLogger } from '@common/observability/logger';
 
 async function bootstrap() {
-  // Global Configuration
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = AppModule.CONFIGURATION.GLOBAL_PREFIX;
-  app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  try {
+    const app = await NestFactory.create(AppModule, {
+      rawBody: true,
+      bufferLogs: true,
+    });
+    // app.useLogger(app.get(PinoLogger));
+    const globalPrefix = AppModule.CONFIGURATION.GLOBAL_PREFIX;
+    app.setGlobalPrefix(globalPrefix);
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  // Swagger Documentation
-  const config = new DocumentBuilder()
-    .setTitle('EInvoice-bff API')
-    .setDescription('The E-bff API description')
-    .setVersion('1.0.0')
-    .addBearerAuth({
-      description: 'Default JWT Authorization',
-      type: 'http',
-      in: 'header',
-      scheme: 'bearer',
-      bearerFormat: 'JWT',
-      name: 'Authorization',
-    })
-    .build();
+    app.enableCors({
+      origin: '*',
+    });
 
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory);
-  const port = AppModule.CONFIGURATION.APP_CONFIG.PORT;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+    const config = new DocumentBuilder()
+      .setTitle('Einvoice-bff API')
+      .setDescription('The Einvoice-bff API description')
+      .setVersion('1.0.0')
+      .addBearerAuth({
+        description: 'Default JWT Authorization',
+        type: 'http',
+        in: 'header',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+      })
+      .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory);
+
+    const port = AppModule.CONFIGURATION.APP_CONFIG.PORT;
+    await app.listen(port);
+    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}/docs`);
+  } catch (error) {
+    Logger.error(`❌ Application failed to start: ${error}`, '', 'Bootstrap', false);
+  }
 }
 
 bootstrap();
