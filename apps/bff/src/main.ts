24fe3fae31bea0a1683d56417c1776f7ad91a887
Sotatek-LongPham2 <long.pham2@sotatek.com>
2026-03-30 21:56:17 +0700

    set up swagger docs

--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -3,14 +3,35 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  // Global Configuration
   const app = await NestFactory.create(AppModule);
   const globalPrefix = AppModule.CONFIGURATION.GLOBAL_PREFIX;
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  // Swagger Documentation
+  const config = new DocumentBuilder()
+    .setTitle('EInvoice-bff API')
+    .setDescription('The E-bff API description')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      description: 'Default JWT Authorization',
+      type: 'http',
+      in: 'header',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'Authorization',
+    })
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory);
   const port = AppModule.CONFIGURATION.APP_CONFIG.PORT;
   await app.listen(port);
   Logger.log(
--- libs/configuration/src/lib/base.config.ts
+++ libs/configuration/src/lib/base.config.ts
@@ -1,4 +1,5 @@
-import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+import { Logger } from '@nestjs/common';
+import { IsBoolean, IsNotEmpty, IsString, validateSync } from 'class-validator';
 
 export class BaseConfiguration {
   @IsString()
@@ -15,5 +16,18 @@ export class BaseConfiguration {
     this.NODE_ENV = process.env['NODE_ENV'] || 'development';
     this.IS_DEV = process.env['NODE_ENV'] === 'development';
     this.GLOBAL_PREFIX = process.env['GLOBAL_PREFIX'] || 'api/v1';
+  }
+
+  validate() {
+    const errors = validateSync(this);
+    if (errors.length > 0) {
+      const _errors = errors.map((error) => {
+        return error.children;
+      });
+
+      Logger.error(_errors, errors);
+
+      throw new Error('Configuration is invalid');
+    }
   }
 }
